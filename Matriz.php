46b7850989374fbb73bd4394b1783c3a30f4d825
Kairo806 <kairo.alvessilva@outlook.com>
2021-09-28 20:07:56 -0300

    criei meu repositorio

---
+++ Matriz.php
@@ -0,0 +1,28 @@
+<?php 
+
+$matriz_1 = [
+
+    [2,5,8],
+    [11,14,17],
+    [20,23,27]
+];
+
+$matriz_2 = [
+
+    [22,33,44],
+    [55,66,77],
+    [88,99,100]
+];
+
+function valida($matriz_1){
+    echo "Olá\n";
+    echo $matriz_1;
+
+}
+
+$matriz_1 = array(2,5,8,11,14,17,20,23,27);
+print_r($matriz_1);
+
+valida ("$matriz_1");
+
+?>
